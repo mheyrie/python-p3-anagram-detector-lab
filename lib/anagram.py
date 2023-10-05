2b7b0573e1c714343e571acd58b731f8f9bb2a42
Mary <omota4fidelity@gmail.com>
2023-10-06 00:18:36 +0100

    updated

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,34 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        
+        word_ 
+        sorted = sorted(self.word.lower())
+
+        anagrams = []
+
+        #iterating over list of words in word 
+        for word in word_list:
+            
+            current_word_sorted = sorted(word.lower())
+
+            
+            if current_word_sorted == word_sorted and self.word.lower() != word.lower():
+                anagrams.append(word)
+
+        return anagrams
+
+
+target_word = "Mary"
+word_list = ['enlists']
+
+anagram = Anagram(target_word)
+matches = anagram.match(word_list)
+
+print(matches)
+
+
+
